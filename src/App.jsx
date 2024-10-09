e776b6cc71784c0a64e768eb39a7cdef58b02b63
Kamronbek Olimovgit config --user.namegit config user.namegit config --global user.name Kamronbek <kamronbekolimov116@gmail.com>
2024-10-10 00:46:53 +0500

    wdfs

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,84 @@
-import { useState } from 'react'
-import './App.css'
-import { useEffect } from 'react'
-import Navbar from './components/navbar/Navbar'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Countries from './pages/countries/Countries'
+import { useState, useEffect } from 'react';
+import './App.css';
+import Navbar from './components/navbar/Navbar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Countries from './pages/countries/Countries';
 function App() {
-  const [API, setAPI] = useState('https://restcountries.com/v3.1/all')
-  const [countries, setCountries] = useState([])
-  const [continentCountry, setContinentCountry] = useState([])
+  const [API, setAPI] = useState('https://restcountries.com/v3.1/all');
+  const [countries, setCountries] = useState([]);
+  const [countriesBase, setCountriesBase] = useState([]);
+  const [mode, setMode] = useState(localStorage.getItem('mode') == 'dark');
+  const modeIcon = mode ? '/darkMoon.svg' : '/lightMoon.svg';
+  const searchIcon = mode ? '/darkSearch.svg' : '/lightSearch.svg';
+  const [continents, setContinents] = useState([
+    'All',
+    'Africa',
+    'Antarctic',
+    'Asia',
+    'Europe',
+    'Americas',
+    'Oceania',
+  ]);
+  const [continentIndex, setContinentIndex] = useState(0);
+  const [openSelect, setOpenSelect] = useState(false);
   const getData = async (api) => {
-    const req = await fetch(api)
-    const data = await req.json()
-    setCountries(data)
-  }
-  const [mode, setMode] = useState(false)
-  const [modeImg, setModeImg] = useState('/lightMoon.svg')
-  const [search, setSearch] = useState('/lightSearch.svg')
-  const [continents, setContinents] = useState(["All","Africa","Antarctic","Asia","Europe", "Americas","Oceania"])
-  const [continentBtn, setContinentBtn] = useState(false)
-  const [continentIndex, setContinentIndex] = useState(0)
-  const changeMode = () => {
-    setMode(!mode)
-    if(mode){
-      setSearch('/lightSearch.svg')
-      setModeImg('/lightMoon.svg')
-    }else{
-      setSearch('/darkSearch.svg')
-      setModeImg('/darkMoon.svg')
-    }
-  }
-  const selectBtn = () => {
-    setContinentBtn(!continentBtn)
-  }
-  const continentFilter = (index) => {
-    setContinentIndex(index);
-    const filterCountries = countries.filter((country) => {
-      switch(continents[index]){
-        case 'All': return country.region != "All"
-        case 'Africa': return country.region == "Africa"
-        case 'Antarctic': return country.region == "Antarctic"
-        case 'Asia': return country.region == "Asia"
-        case 'Europe': return country.region == "Europe"
-        case 'Americas': return country.region == "Americas"
-        case 'Oceania': return country.region == "Oceania"
-        default: return true
-      }
-    });
-    setContinentCountry(filterCountries)
+    const req = await fetch(api);
+    const data = await req.json();
+    setCountries(data);
+    setCountriesBase(data);
   };
-  const [searchCountry, setSearchCountry] = useState('');
-  const searchInput = (inputValue) => {
-    setSearchCountry(inputValue)
-  }
-  useEffect(()=>{
-    getData(API)
-    continentFilter(continentIndex)
-  },[API, continentIndex, countries])
+  const changeMode = () => {
+    setMode((prevMode) => {
+      const newMode = !prevMode;
+      localStorage.setItem('mode', newMode ? 'dark' : 'light');
+      return newMode;
+    });
+  };
+  const searchFunction = (name) => {
+    const searchCountries = countries.filter((country) => {
+      return (
+        country.name.common.toLowerCase().includes(name.toLowerCase().trim()) &&
+        (continents[continentIndex] === 'All' || country.region === continents[continentIndex])
+      );
+    });
+    setCountriesBase(searchCountries);
+  };
+  const selectOpenFunction = () => {
+    setOpenSelect(!openSelect);
+  };
+  const selectFunction = (index) => {
+    setContinentIndex(index);
+    const selectCountries = countries.filter((country) => {
+      return continents[index] == 'All' || country.region == continents[index];
+    });
+    setCountriesBase(selectCountries);
+  };
+  useEffect(() => {
+    getData(API);
+  }, [API]);
   return (
-    <div className={mode?'body dark':'body'}>
-    <BrowserRouter>
-    <Navbar changeMode={changeMode} modeImg={modeImg}/>
-    <Routes>
-      <Route path='/' element={<Countries continentCountry={continentCountry} continentIndex={continentIndex} continentBtn={continentBtn} selectBtn={selectBtn} search={search} continents={continents} continentFilter={continentFilter} searchInput={searchInput} searchCountry={searchCountry}/>}/>
-    </Routes>
-    </BrowserRouter>
+    <div className={mode ? 'body dark' : 'body'}>
+      <BrowserRouter>
+        <Navbar changeMode={changeMode} modeImg={modeIcon} />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Countries
+                countriesBase={countriesBase}
+                searchFunction={searchFunction}
+                search={searchIcon}
+                continents={continents}
+                selectFunction={selectFunction}
+                selectOpenFunction={selectOpenFunction}
+                openSelect={openSelect}
+                continentIndex={continentIndex}
+              />
+            }
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
-export default App
+export default App;
--- src/pages/countries/Countries.jsx
+++ src/pages/countries/Countries.jsx
@@ -1,15 +1,13 @@
-import React, { useState } from "react";
 import "./Countries.css";
 function Countries({
-  continentCountry,
+  countriesBase,
   search,
+  searchFunction,
   continents,
-  continentFilter,
-  selectBtn,
-  continentBtn,
+  selectFunction,
+  selectOpenFunction,
+  openSelect,
   continentIndex,
-  searchInput,
-  searchCountry
 }) 
 {
   return (
@@ -21,51 +19,45 @@ function Countries({
               <img src={search} alt="Search Icon" />
             </button>
             <input
+              onInput={(e)=>searchFunction(e.target.value)}
               type="text"
-              placeholder="Search for a country…"
-              value={searchCountry}
-              onInput={(e) => searchInput(e.target.value)}
-            />
+              placeholder="Search for a country…"/>
           </form>
-          <button
-            onClick={() => selectBtn()}
-            className={continentBtn ? "select active" : "select"}>
+          <button onClick={()=>selectOpenFunction()} className={openSelect?'select active':'select'}>
             <span>{continents[continentIndex]}</span>
             <i className="fa-solid fa-chevron-up"></i>
             <div className="continent">
-              {continents.map((continent, index) => (
-                <div onClick={() => continentFilter(index)} key={index}>
-                  {continent}
-                </div>
-              ))}
+              {
+                continents.map((continent, index)=>{
+                  return <div onClick={()=>selectFunction(index)} key={index}>{continent}</div>
+                })
+              }
             </div>
           </button>
         </div>
         <div className="countriesPage">
-          {continentCountry.map((country, index) => {
-            return (
-              <div key={index} className="country">
-                <img src={country.flags.svg} alt={country.flags.alt} />
-                <div className="countryInfo">
-                  <h3>{country.name.common}</h3>
-                  <div>
-                    <p>
-                      <b>Population: </b>
-                      {country.population.toLocaleString("ru")}
-                    </p>
-                    <p>
-                      <b>Region: </b>
-                      {country.region}
-                    </p>
-                    <p>
-                      <b>Capital: </b>
-                      {country.capital?country.capital:'No capital'}
-                    </p>
-                  </div>
+          {countriesBase.length?countriesBase.map((country, index) => {            
+            return <div key={index} className="country">
+              <img src={country.flags.svg} alt={country.flags.alt} />
+              <div className="countryInfo">
+                <h3>{country.name.common}</h3>
+                <div>
+                  <p>
+                    <b>Population: </b>
+                    {country.population.toLocaleString("ru")}
+                  </p>
+                  <p>
+                    <b>Region: </b>
+                    {country.region}
+                  </p>
+                  <p>
+                    <b>Capital: </b>
+                    {country.capital?country.capital:'No capital'}
+                  </p>
                 </div>
               </div>
-            );
-          })}
+            </div>
+          }):<h1>Not found country</h1>}
         </div>
       </div>
     </div>
