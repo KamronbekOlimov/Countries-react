709f550e8008ee5ab44b9481d7675d2ee36afe1d
Kamronbek Olimovgit config --user.namegit config user.namegit config --global user.name Kamronbek <kamronbekolimov116@gmail.com>
2024-10-14 07:46:10 +0500

    efegfr

--- src/pages/oneCountry/OneCountry.jsx
+++ src/pages/oneCountry/OneCountry.jsx
@@ -3,8 +3,7 @@ import "./OneCountry.css";
 function OneCountry({ countriesBase }) {
   const { name } = useParams();
   const navigate = useNavigate();
-  const country = countriesBase.find((item) => item.name.common == name);
-  console.log(country);
+  const country = countriesBase.filter((item) => item.name.common === name)[0];
   return (
     <div className="oneCountry">
       <div className="container">
@@ -12,30 +11,35 @@ function OneCountry({ countriesBase }) {
           <i className="fa-solid fa-arrow-left"></i>
           <span>Back</span>
         </button>
-        <div className="countryInfo">
-          <img src={country.flags.svg} alt={`Flag of ${country.name.common}`} />
-          <div className="countryDetails">
-            <h1>{country.name.common}</h1>
-            <div>
-            <p>
-              <b>Region:</b> {country.region}
-            </p>
-            <p>
-              <b>Population:</b> {country.population.toLocaleString()}
-            </p>
-            <p>
-              <b>Capital:</b>{" "}
-              {country.capital ? country.capital[0] : "No capital"}
-            </p>
-            <p>
-              <b>Subregion:</b> {country.subregion}
-            </p>
-            <p>
-              <b>Native name:</b> {country.name.official}
-            </p>
+          {
+            country ? <div className="countryInfo">
+            <img
+              src={country.flags.svg}
+              alt={`Flag of ${country.name.common}`}
+            />
+            <div className="countryDetails">
+              <h1>{country.name.common}</h1>
+              <div>
+                <p>
+                  <b>Region:</b> {country.region}
+                </p>
+                <p>
+                  <b>Population:</b> {country.population.toLocaleString()}
+                </p>
+                <p>
+                  <b>Capital:</b>{" "}
+                  {country.capital ? country.capital[0] : "No capital"}
+                </p>
+                <p>
+                  <b>Subregion:</b> {country.subregion}
+                </p>
+                <p>
+                  <b>Native name:</b> {country.name.official}
+                </p>
+              </div>
             </div>
-          </div>
-        </div>
+          </div> : <h1>Not found country</h1>
+          }
       </div>
     </div>
   );
